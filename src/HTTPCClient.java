63ff3dcfd7b2fbbc863b1596e5cd4f09679c3172
rohitsharma <rohit.sharma06596@gmail.com>
2019-09-28 09:17:16 -0400

    HTTPCClient updated

--- src/HTTPCClient.java
+++ src/HTTPCClient.java
@@ -1,70 +1,14 @@
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.SocketAddress;
 import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+
 
 public class HTTPCClient {
 
-    // readFully reads until the request is fulfilled or the socket is closed
-    private static void readFully(SocketChannel socket, ByteBuffer buf, int size) throws IOException {
-        while (buf.position() < size) {
-            int n = socket.read(buf);
-            if (n == -1) {
-                break;
-            }
-        }
-        if (buf.position() != size) {
-            throw new EOFException();
-        }
-    }
-
-    private static void readEchoAndRepeat(SocketChannel socket) throws IOException {
-        Charset utf8 = StandardCharsets.UTF_8;
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            ByteBuffer buf = utf8.encode(line);
-            int n = socket.write(buf);
-            buf.clear();
-
-            // Receive all what we have sent
-            readFully(socket, buf, n);
-            buf.flip();
-            System.out.println("Replied: " + utf8.decode(buf));
-        }
-    }
-
-    private static void runClient(SocketAddress endpoint) throws IOException {
-        try (SocketChannel socket = SocketChannel.open()) {
-            socket.connect(endpoint);
-            System.out.println("Type any thing then ENTER. Press Ctrl+C to terminate");
-            readEchoAndRepeat(socket);
-        }
-    }
 
     public static void main(String[] args) throws IOException {
-//        OptionParser parser = new OptionParser();
-//        parser.acceptsAll(asList("host", "h"), "EchoServer hostname")
-//                .withOptionalArg()
-//                .defaultsTo("localhost");
-//
-//        parser.acceptsAll(asList("port", "p"), "EchoServer listening port")
-//                .withOptionalArg()
-//                .defaultsTo("8007");
-//
-//        OptionSet opts = parser.parse(args);
-//
-//        String host = (String) opts.valueOf("host");
-//        int port = Integer.parseInt((String) opts.valueOf("port"));
-//
-//        SocketAddress endpoint = new InetSocketAddress(host, port);
-//        runClient(endpoint);
-        //MyGETRequest();
+
+        MyGETRequest();
         MyPOSTRequest();
     }
 
@@ -73,7 +17,7 @@ public class HTTPCClient {
         String readLine = null;
         HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
         conection.setRequestMethod("GET");
-        conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
+        //conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
         int responseCode = conection.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in = new BufferedReader(
